b8c2091ab2b22da73403b91a3b3a406129257049
harishkannarao <harish.gtec@gmail.com>
2018-12-19 09:44:45 +0000

    Using typed functions

--- src/main/kotlin/com/harishkannarao/http/web/ExamplePageWebClient.kt
+++ src/main/kotlin/com/harishkannarao/http/web/ExamplePageWebClient.kt
@@ -3,6 +3,7 @@ package com.harishkannarao.http.web
 import com.gargoylesoftware.htmlunit.WebClient
 import com.gargoylesoftware.htmlunit.html.*
 import org.awaitility.kotlin.await
+import java.lang.IllegalStateException
 import java.util.concurrent.TimeUnit
 
 class ExamplePageWebClient(
@@ -15,21 +16,39 @@ class ExamplePageWebClient(
         return heading.asText()
     }
 
-    private fun getDivElement(htmlPage: HtmlPage): HtmlDivision {
-        val divs = { htmlPage.querySelectorAll("div") }
-        await.alias("Wait for div element")
-                .atMost(2L, TimeUnit.SECONDS)
-                .pollInterval(100L, TimeUnit.MILLISECONDS)
-                .until { divs().firstOrNull()?.isDisplayed }
-        return divs().first() as HtmlDivision
+    private fun getDivElement(node: DomNode): HtmlDivision {
+        val cssSelector = "div"
+        awaitElementDisplay(node, cssSelector)
+        return getMandatoryElement(node, cssSelector)
     }
 
-    private fun getH1Element(domElement: DomElement): HtmlHeading1 {
-        val headings = { domElement.querySelectorAll("h1") }
-        await.alias("Wait for heading element")
+    private fun getH1Element(node: DomNode): HtmlHeading1 {
+        val cssSelector = "h1"
+        awaitElementDisplay(node, cssSelector)
+        return getMandatoryElement(node, cssSelector)
+    }
+
+    private fun awaitElementDisplay(node: DomNode, cssSelector: String) {
+        val elementQuery: (DomNode) -> List<DomNode> = {
+            getNodesByCssSelector(it, cssSelector)
+        }
+        await.alias("Wait for '$cssSelector' element to be displayed on page")
                 .atMost(2L, TimeUnit.SECONDS)
                 .pollInterval(100L, TimeUnit.MILLISECONDS)
-                .until { headings().firstOrNull()?.isDisplayed }
-        return headings().first() as HtmlHeading1
+                .until { elementQuery(node).firstOrNull()?.isDisplayed }
     }
+
+    private fun getNodesByCssSelector(node: DomNode, cssSelector: String): List<DomNode> {
+        return node.querySelectorAll(cssSelector).toList()
+    }
+
+    private fun <T : DomNode> getElement(node: DomNode, cssSelector: String): T? {
+        return node.querySelector(cssSelector) as T?
+    }
+
+    private fun <T : DomNode> getMandatoryElement(node: DomNode, cssSelector: String): T {
+        return getElement(node, cssSelector)
+                ?: throw IllegalStateException("'$cssSelector' element not found on page")
+    }
+
 }
